f614273ae7c47286cc1a5ba4fb1add57d7a95881
fentyhall <fentymhall@gmail.com>
2020-10-02 13:19:37 -0700

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -3,11 +3,11 @@ def selects_all_female_bears_return_name_and_age
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "SELECT name FROM bears ORDER BY name ASC;"
+  "SELECT name FROM bears ORDER BY name;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT name, age FROM bears WHERE alive = 1 ORDER BY age ASC;"
+  "SELECT name, age FROM bears WHERE alive = 1 ORDER BY age;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
@@ -16,7 +16,7 @@ def selects_oldest_bear_and_returns_name_and_age
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears ORDER BY age ASC LIMIT 1;"
+  "SELECT name, age FROM bears ORDER BY age LIMIT 1;"
   # "SELECT name, MIN(age) FROM bears;"
 end
 
